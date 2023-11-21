233f3727019c94735f689d451ebc010518890e01
Osián <149536063+OsianJL@users.noreply.github.com>
2023-11-21 17:19:43 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Osian in da flat ");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["my", "the", "a"];
+  let adverb = ["cat", "dog", "racoon", "cop", "driver", "lion"];
+  let action = ["ate", "destroyed", "burned", "hide", "threw", "stole"];
+  let adjective = ["my"];
+  let noun = ["homework", "bike", "car", "bus card"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let advIndex = Math.floor(Math.random() * adverb.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let adjIndex = Math.floor(Math.random() * adjective.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  return (
+    pronoun[proIndex] +
+    " " +
+    adverb[advIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    adjective[adjIndex] +
+    " " +
+    noun[nounIndex]
+  );
 };
